ea64fdf41765cf7ded7062a9fcdaf8009a110df8
Pascal Stammer <stammer@deichbrise.de>
2022-11-08 16:26:52 +0100

    remove

--- shell.nix
+++ shell.nix
@@ -24,44 +24,4 @@ stdenv.mkDerivation {
         terraform
         kube3d
     ];
-
-    shellHook = ''
-
-        function setup() {
-
-            alias kubectl='minikube kubectl'
-            alias skaffoldx='./.skaffoldx'
-            . <(minikube completion bash)
-            . <(helm completion bash)
-
-            minikube start -p technilog-platform --kubernetes-version=v1.22.15
-            minikube addons enable ingress -p technilog-platform
-            kubectl apply -k github.com/zalando/postgres-operator/manifests
-            kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
-            kubectl create namespace kafka
-            kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
-
-            git clone https://github.com/keycloak/keycloak-operator.git
-            kubectl apply -f keycloak-operator/deploy/crds/
-            kubectl create namespace keycloak-operator
-            kubectl apply -f keycloak-operator/deploy/role.yaml -n keycloak-operator
-            kubectl apply -f keycloak-operator/deploy/role_binding.yaml -n keycloak-operator
-            kubectl apply -f keycloak-operator/deploy/service_account.yaml -n keycloak-operator
-            kubectl apply -f keycloak-operator/deploy/operator.yaml -n keycloak-operator
-            rm -rf keycloak-operator
-
-            git clone https://github.com/rook/rook.git
-
-            kubectl create -f rook/deploy/examples/crds.yaml
-            kubectl create -f rook/deploy/examples/common.yaml
-            kubectl create -f rook/deploy/examples/operator.yaml
-            kubectl create -f rook/deploy/examples/cluster.yaml
-
-            rm -rf rook
-
-            source <(minikube docker-env -p technilog-platform)
-
-            skaffold config set --kube-context technilog-platform local-cluster true
-        }
-      '';
 }
